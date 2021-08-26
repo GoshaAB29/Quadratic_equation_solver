23cee87d2b1d7ccaa4a9cb2cce6d99a52f958d43
Gosha <89145061+GoshaAB29@users.noreply.github.com>
2021-08-26 21:58:17 +0300

    Add files

---
+++ header/input_square.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <assert.h>
+
+void init_square (double *a, double *b, double *c);
---
+++ header/output_square.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <assert.h>
+
+void output_square (int kolvoans, double *ans1, double *ans2)
+
