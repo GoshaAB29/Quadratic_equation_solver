0228a68dda6abc947f19eb4a321083bd09804f63
Gosha <89145061+GoshaAB29@users.noreply.github.com>
2021-08-27 16:59:05 +0300

    fix bugs

--- header/input_square.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <assert.h>
-
-void init_square (double *a, double *b, double *c);
--- header/output_square.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <assert.h>
-
-void output_square (int kolvoans, double *ans1, double *ans2);
-
