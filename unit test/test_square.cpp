c41070f1cb92ba4dc0bf11a10ea7ace906dfd364
Gosha <89145061+GoshaAB29@users.noreply.github.com>
2021-08-28 15:54:03 +0300

    update unit test

--- unit test/test_square.cpp
+++ unit test/test_square.cpp
@@ -1,50 +1,109 @@
 #include "../unit test/test_square.h"
 
-void test_output (int test_num, double x1, double x2)
+square_equation tests[6];
+
+void tests_init ()
 {
-            switch (test_num) {
-            case 0:
-                break;
-            case 1:
-                printf ("x = %lf", x1);
-                break;
-            case 2:
-                printf ("x1 = %lf, x2 = %lf", x1, x2);
-                break;
-            default:
-                printf ("ERROR: the number of answers is incorrect");
-            }
+    tests[0].test_num = 1;
+    tests[0].num_ans_true = INF_ANS;
+    tests[0].ans1_true = NAN;
+    tests[0].ans2_true = NAN;
+    tests[0].a = 0;
+    tests[0].b = 0;
+    tests[0].c = 0;
+
+    tests[1].test_num = 2;
+    tests[1].num_ans_true = ZERO_ANS;
+    tests[1].ans1_true = NAN;
+    tests[1].ans2_true = NAN;
+    tests[1].a = 0;
+    tests[1].b = 0;
+    tests[1].c = 3;
+
+    tests[2].test_num = 3;
+    tests[2].num_ans_true = 1;
+    tests[2].ans1_true = 0.5;
+    tests[2].ans2_true = NAN;
+    tests[2].a = 0;
+    tests[2].b = 2;
+    tests[2].c = -1;
+
+    tests[3].test_num = 4;
+    tests[3].num_ans_true = ZERO_ANS;
+    tests[3].ans1_true = NAN;
+    tests[3].ans2_true = NAN;
+    tests[3].a = 5;
+    tests[3].b = -6;
+    tests[3].c = 7;
+
+    tests[4].test_num = 5;
+    tests[4].num_ans_true = 1;
+    tests[4].ans1_true = -1;
+    tests[4].ans2_true = NAN;
+    tests[4].a = 1;
+    tests[4].b = 2;
+    tests[4].c = 1;
+
+    tests[5].test_num = 6;
+    tests[5].num_ans_true = 2;
+    tests[5].ans1_true = 4;
+    tests[5].ans2_true = -2;
+    tests[5].a = 1;
+    tests[5].b = -2;
+    tests[5].c = -8;
 }
 
-int UnitTest_Square (int test_num, int num_ans_best, double ans1_best,
-                     double ans2_best, double a, double b, double c) {
+void test_output (int test_num, double x1, double x2)
+{
+    switch (test_num) {
+        case 0:
+            printf ("\n");
+            break;
+
+        case 1:
+            printf ("x = %.3lf;\n", x1);
+            break;
+
+        case 2:
+            printf ("x1 = %.3lf, x2 = %.3lf;\n", x1, x2);
+            break;
+
+        default:
+            printf ("ERROR: the number of answers is incorrect\n");
+        }
+}
+
+int unit_test_square (square_equation tests) {
     double ans1 = NAN, ans2 = NAN;
-    int num_ans = solve_square (a, b, c, &ans1, &ans2);
 
-    if ((!is_equal (num_ans, num_ans_best)) ||
-        (!is_equal (ans1, ans1_best)) || (!is_equal (ans2, ans2_best))) {
+    int num_ans = solve_square (tests.a, tests.b, tests.c, &ans1, &ans2);
 
-            printf ("Test %d failed, in result: %d answers.", test_num, num_ans);
+    if ((!is_equal (num_ans, tests.num_ans_true)) ||
+        (!is_equal (ans1, tests.ans1_true)) || (!is_equal (ans2, tests.ans2_true))) {
+
+            printf ("Test %d failed, in result: %d answers. ", tests.test_num, num_ans);
             test_output (num_ans, ans1, ans2);
 
-            printf ("Should be: %d answers", num_ans_best);
-            test_output (num_ans_best, ans1_best, ans2_best);
+            printf ("Test is %.3lfx^2 + %.3lfx + %.3lf.\n", tests.a, tests.b, tests.c);
+
+            printf ("Should be: %d answers, ", tests.num_ans_true);
+            test_output (tests.num_ans_true, tests.ans1_true, tests.ans2_true);
 
             return 1;
     } else
     return 0;
 }
 
-int Run_Unit_Tests ()
+int run_unit_tests ()
 {
     int failed = 0;
 
-    failed = failed + UnitTest_Square (1, INF_ANS, NAN, NAN, 0, 0, 0);
-    failed = failed + UnitTest_Square (2, ZERO_ANS, NAN, NAN, 0, 0, 3);
-    failed = failed + UnitTest_Square (3, 0, NAN, NAN, 5, -6, 7);
-    failed = failed + UnitTest_Square (5, 1, -1, NAN, 1, 2, 1);
-    failed = failed + UnitTest_Square (6, 2, 4, -2, 1, -2, -8);
-    failed = failed + UnitTest_Square (7, 1, -0.500, NAN, 0, 2, 1);
+    tests_init ();
+
+    for (int number_of_test = 0; number_of_test < 6; ++number_of_test)
+        failed += unit_test_square (tests[number_of_test]);
+
+    printf ("Testing errors: %d\n", failed);
 
     return failed;
 }
--- unit test/test_square.h
+++ unit test/test_square.h
@@ -1,11 +1,54 @@
+/// \brief Function testing for correctness
 #pragma once
 #include <stdio.h>
 #include <math.h>
 #include "../header/solve_square.h"
 
+/// test values ​​needed to check the correctness program
+struct square_equation
+{
+int test_num;                                      ///< test equation number
+int num_ans_true;                                  ///< correct number of roots in the equation
+double ans1_true;                                  ///< correct value of the 1st answer of the equation
+double ans2_true;                                  ///< correct value of the 2st answer of the equation
+double a;                                          ///< coefficient in front of x^2
+double b;                                          ///< coefficient in front of x
+double c;                                          ///< free coefficient
+
+};
+
+///initialization of tests
+void test_init ();
+
+//-----------------------------------------------
+//! Print of roots depending on their number
+//!
+//! @param [in] test_num - number of answers
+//! @param [in] x1, x2 - roots of the equation
+//!
+//-----------------------------------------------
+
 void test_output (int test_num, double x1, double x2);
 
-int UnitTest_Square (int test_num, int num_ans_best, double ans1_best,
-                     double ans2_best, double a, double b, double c);
+//-----------------------------------------------
+//! Calculating roots and verifying the answers
+//!
+//! @param [in] test_num - number of answers (in program)
+//! @param [in] num_ans_best - number of answers ( in test)
+//! @param [in] ans1_best, ans2_best - answers of the equation (in test)
+//! @param [in] a, b, c - coefficients
+//!
+//! @return 1 if error and 0 if true
+//!
+//-----------------------------------------------
 
-int Run_Unit_Tests ();
+int unit_test_square (square_equation tests);
+
+//-----------------------------------------------
+//! Calling tests and counting ERRORs
+//!
+//!  @return failed - number of failed tests
+//!
+//-----------------------------------------------
+
+int run_unit_tests ();
