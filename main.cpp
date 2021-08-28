34263a2076551f3a85e6bd6d72740a7dc91b961a
Gosha <89145061+GoshaAB29@users.noreply.github.com>
2021-08-28 15:53:02 +0300

    update functions, add struct

--- main.cpp
+++ main.cpp
@@ -1,22 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 #include "header/input_square.h"
 #include "header/solve_square.h"
 #include "header/output_square.h"
 #include "unit test/test_square.h"
 
+//! main function
+
 int main ()
 {
-    int test = Run_Unit_Tests ();
-
-    printf ("%d\n", test);
+    int test = run_unit_tests ();
 
     double a = NAN, b = NAN, c = NAN;
-    init_square (&a, &b, &c);
+    input_square (&a, &b, &c);
 
-    double  ans1 = 0, ans2 = 0;
+    double  ans1 = NAN, ans2 = NAN;
     int num_ans = solve_square (a, b, c, &ans1, &ans2);
+
     output_square (num_ans, &ans1, &ans2);
 
     return 0;
